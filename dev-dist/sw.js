510543760835629cdb43c5fb936e066a55473368
[dyad] <riodedinheirorj@gmail.com>
2025-12-03 09:29:27 -0300

    [dyad] Corrigindo nomes e criando ícones para PWA e Vercel - wrote 3 file(s), renamed 2 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-1b3d9405'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.p6rlpdr2tq8"
+    "revision": "0.cihqjopr3t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
