147420bed85ade7f57adc93e8c4e8afd125d1bb9
[dyad] <riodedinheirorj@gmail.com>
2025-12-13 20:08:15 -0300

    [dyad] Configuração de ícones PWA explicada - wrote 3 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-c46fe8f7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.grsjhci98cg"
+    "revision": "0.i3t64fpicto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
